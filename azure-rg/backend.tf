bc00f9a4f76f2d09e062d21efe6248f289170136
KapilYadava <kapilyadava.isa@gmail.com>
2022-08-06 15:44:43 +0530

    adding aks resource

--- azure-rg/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name = "azuretestrg"
-    storage_account_name = "myteststorage1989"
-    container_name= "mytestcontainer19890"
-    key= "demotf.terraform.tfstate"
-    }
-}
--- azure-rg/main.tf
+++ azure-rg/main.tf
@@ -25,3 +25,35 @@ resource "azurerm_storage_container" "my-test-container" {
     azurerm_storage_account.my-storage
   ]
 }
+
+resource "azurerm_container_registry" "acr" {
+  name                = "myazureregistry1989"
+  resource_group_name = azurerm_resource_group.my-rg.name
+  location            = azurerm_resource_group.my-rg.location
+  sku                 = "Premium"
+  admin_enabled       = false
+  depends_on = [
+    azurerm_storage_account.my-storage
+  ]
+}
+
+resource "azurerm_kubernetes_cluster" "my-cluster" {
+  name                = "myaks"
+  location            = azurerm_resource_group.my-rg.location
+  resource_group_name = azurerm_resource_group.my-rg.name
+  dns_prefix          = "my-aks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  depends_on = [
+    azurerm_storage_account.my-storage
+  ]
+}
